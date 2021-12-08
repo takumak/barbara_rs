1158da0c57c7b8c0bdd027bcf725979a9eae41d4
takumak <r9ag6fx0adr@gmail.com>
2021-12-08 16:31:08 +0000

    backtrace: use decl_c_symbol_addr macro

--- src/backtrace.rs
+++ src/backtrace.rs
@@ -12,44 +12,37 @@ function:
 - https://developer.arm.com/documentation/100067/0607/armclang-Command-line-Options/-fomit-frame-pointer---fno-omit-frame-pointer
 - linux/arch/arm64/kernel/stacktrace.c
 
-*/
+ */
 
+use crate::decl_c_symbol_addr;
+decl_c_symbol_addr!(__text_s, text_s);
+decl_c_symbol_addr!(__text_e, text_e);
+decl_c_symbol_addr!(__stack_s, stack_s);
+decl_c_symbol_addr!(__stack_e, stack_e);
+
+#[derive(Clone, Copy)]
 struct StackFrame {
     fp: usize,
     lr: usize,
 }
 
 pub fn unwind_walk(pc: usize, fp: usize, limit: u32, func: fn(usize)) {
-    unsafe {
-        extern "C" {
-            static __text_s: u8;
-            static __text_e: u8;
-            static __stack_s: u8;
-            static __stack_e: u8;
+    let mut fp_ = fp;
+
+    func(pc);
+
+    for _i in 1..limit {
+        if fp_ < stack_s() || fp_ >= stack_e() {
+            break;
         }
 
-        let text_s = &__text_s as *const u8 as usize;
-        let text_e = &__text_e as *const u8 as usize;
-        let stack_s = &__stack_s as *const u8 as usize;
-        let stack_e = &__stack_e as *const u8 as usize;
-
-        let mut fp_ = fp;
-
-        func(pc);
-
-        for _i in 1..limit {
-            if fp_ < stack_s || fp_ >= stack_e {
-                break;
-            }
-
-            let ref frame: StackFrame = *(fp_ as *const StackFrame);
-            if frame.lr < text_s || frame.lr >= text_e {
-                break;
-            }
-
-            func(frame.lr);
-            fp_ = frame.fp;
+        let frame: StackFrame = unsafe { *(fp_ as *const StackFrame) };
+        if frame.lr < text_s() || frame.lr >= text_e() {
+            break;
         }
+
+        func(frame.lr);
+        fp_ = frame.fp;
     }
 }
 
