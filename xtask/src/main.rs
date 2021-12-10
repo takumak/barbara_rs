37e40d4308509d98d1deb0f559666f49c2112216
takumak <r9ag6fx0adr@gmail.com>
2021-12-10 14:21:35 +0000

    xtask: rename subcommand {test => testall}

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -20,7 +20,7 @@ enum Commands {
         #[clap(last = true)]
         args: Vec<String>,
     },
-    Test {
+    Testall {
         #[clap(last = true)]
         args: Vec<String>,
     },
@@ -38,13 +38,17 @@ fn cargo_target(cmd: &str, args: &Vec<String>) {
     assert!(status.success(), "failed to execute: {:?}", cargo);
 }
 
-fn cargo_test(args: &Vec<String>) {
-    let mut args_all = vec!["test", "-p", "linked_list_allocator"];
+fn cargo_testall(args: &Vec<String>) {
+    let mut args_all = vec![
+        "test",
+        "-p", "kallsyms",
+        "-p", "linked_list_allocator",
+    ];
     args_all.extend(args.iter().map(|s| &**s));
 
     let mut cargo = process::Command::new("cargo");
     cargo.args(args_all);
-    cargo.env("RUST_BACKTRACE", "1");
+    // cargo.env("RUST_BACKTRACE", "1");
 
     let status = cargo.status().unwrap();
     assert!(status.success(), "failed to execute: {:?}", cargo);
@@ -60,8 +64,8 @@ fn main() {
         Some(Commands::Build { args }) => {
             cargo_target("build", args)
         }
-        Some(Commands::Test { args }) => {
-            cargo_test(args)
+        Some(Commands::Testall { args }) => {
+            cargo_testall(args)
         }
         None => {}
     }
