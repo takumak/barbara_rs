35533aa2e3b93dbffe623db5e073fb619a169dcf
takumak <r9ag6fx0adr@gmail.com>
2021-12-21 11:28:59 +0900

    elf_parser: add testcases

--- libs/elf_parser/src/err.rs
+++ libs/elf_parser/src/err.rs
@@ -14,3 +14,24 @@ impl ElfParserError {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::ElfParserError;
+
+    #[test]
+    fn elfparsererror_partialeq() {
+        let err1 = ElfParserError::new(
+            posix::Errno::EINVAL, format!("Test"));
+        let err2 = ElfParserError::new(
+            posix::Errno::EINVAL, format!("Test"));
+        assert_eq!(err1, err2);
+    }
+
+    #[test]
+    fn elfparsererror_debug() {
+        let err = ElfParserError::new(
+            posix::Errno::EINVAL, format!("Test"));
+        assert_eq!(format!("{:?}", err), "ElfParserError { errno: EINVAL, message: \"Test\" }");
+    }
+}
