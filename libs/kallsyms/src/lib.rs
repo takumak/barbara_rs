667182c39b34da808b32e88f6e31dc4217f2b896
takumak <r9ag6fx0adr@gmail.com>
2021-12-25 12:16:43 +0900

    kallsyms: add no_std feature

--- libs/kallsyms/src/lib.rs
+++ libs/kallsyms/src/lib.rs
@@ -1,7 +1,14 @@
-mod compress;
+#![cfg_attr(all(feature = "no_std", not(test)), no_std)]
+
 mod kallsyms;
-mod pack;
 mod types;
 
 pub use crate::kallsyms::KAllSyms;
+pub type Address = types::AddrTblEntry;
+
+#[cfg(not(all(feature = "no_std", not(test))))]
+mod compress;
+#[cfg(not(all(feature = "no_std", not(test))))]
+mod pack;
+#[cfg(not(all(feature = "no_std", not(test))))]
 pub use pack::pack;
