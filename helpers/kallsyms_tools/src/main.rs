f9fa33dce8a8c10d1ad667256f5e05b7759e0b30
takumak <r9ag6fx0adr@gmail.com>
2021-12-25 12:47:12 +0900

    Revert "kallsyms_tools: add `dic` subcommand"
    This reverts commit b83e778d501467191139cb82cafb6111b896c500.

--- helpers/kallsyms_tools/src/main.rs
+++ helpers/kallsyms_tools/src/main.rs
@@ -17,10 +17,6 @@ enum Commands {
         #[clap(value_name = "FILENAME")]
         filename: String,
     },
-    Dic {
-        #[clap(value_name = "FILENAME")]
-        filename: String,
-    },
 }
 
 fn main() {
@@ -31,20 +27,6 @@ fn main() {
             use symbol::symbols_from_file;
             for sym in symbols_from_file(filename) {
                 println!("{:08x} {}", sym.addr, sym.name);
-            }
-        }
-        Some(Commands::Dic { filename }) => {
-            use symbol::symbols_from_file;
-            use compress::make_dic::make_dic;
-
-            let syms: Vec<Vec<u8>> =
-                symbols_from_file(filename)
-                .iter()
-                .map(|s| s.name.as_bytes().to_vec())
-                .collect();
-
-            for (token, count) in make_dic(syms.iter().map(|s| s.as_slice()).collect()) {
-                println!("{} ({})", std::str::from_utf8(&token).unwrap(), count);
             }
         }
         None => {}
