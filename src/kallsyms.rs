cb860401fccfa72dea39924de7bc00a2d3755ba1
takumak <r9ag6fx0adr@gmail.com>
2021-12-25 12:17:12 +0900

    support for new design in kallsyms

--- src/kallsyms.rs
+++ src/kallsyms.rs
@@ -5,5 +5,8 @@ decl_c_symbol_addr!(__kallsyms, kallsyms_addr);
 
 pub fn safe_search<'a>(addr: usize, buf: &'a mut [u8]) -> Option<(&'a str, usize)> {
     let kallsyms = kallsyms::KAllSyms::new(kallsyms_addr());
-    kallsyms.safe_search(addr, buf)
+    match kallsyms.safe_search(addr as kallsyms::Address, buf) {
+        Some((name, off)) => Some((name, off as usize)),
+        None => None,
+    }
 }
