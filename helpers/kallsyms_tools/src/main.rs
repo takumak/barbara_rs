30fd1b37bb17fcba0af269d7d961584347423500
takumak <r9ag6fx0adr@gmail.com>
2021-12-25 13:17:20 +0900

    kallsyms_tools: add `pack` subcommand

--- helpers/kallsyms_tools/src/main.rs
+++ helpers/kallsyms_tools/src/main.rs
@@ -1,8 +1,9 @@
 extern crate clap;
 use clap::{Parser, Subcommand};
 
+extern crate kallsyms;
+
 mod symbol;
-mod compress;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -14,6 +15,11 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     ListSymbols {
+        #[clap(value_name = "FILENAME")]
+        filename: String,
+    },
+
+    Pack {
         #[clap(value_name = "FILENAME")]
         filename: String,
     },
@@ -29,6 +35,29 @@ fn main() {
                 println!("{:08x} {}", sym.addr, sym.name);
             }
         }
+
+        Some(Commands::Pack { filename }) => {
+            use std::io::Write;
+            use symbol::symbols_from_file;
+
+            let symbols: Vec<(String, u32)> =
+                symbols_from_file(filename)
+                .into_iter()
+                .map(|s| (s.name, s.addr))
+                .collect();
+            let data = kallsyms::pack(&symbols);
+            std::io::stdout().write(&data).unwrap();
+
+            let plain_len =
+                (symbols.len() * 6) +
+                symbols.iter()
+                .map(|(name, _addr)| name.as_bytes().len())
+                .reduce(|acc, len| acc + len)
+                .unwrap();
+            eprintln!("compression ratio: {:.2}",
+                      data.len() as f64 / plain_len as f64);
+        }
+
         None => {}
     }
 }
